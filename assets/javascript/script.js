5f8d0c95dd6439c0767bc267435c5bdfcbd4c06e
Shawn Holcomb <sholcmb@gmail.com>
2019-02-14 03:19:43 -0600

    final updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,39 @@
 $(document).ready(function () {
-    $('#menu-button').on('click', function () {
-        $('.menu-box').slideToggle('slow');
-    });
+    // Trigger open /close on mobile
 
-    $('#about-button').on('click', function () {
-        $('.about-box').slideToggle('slow');
-    });
+    if ($(window).width() < 769) {
+        $('#menu-button').on('click', function () {
+            $('.menu-box').slideToggle('slow');
+        });
 
-    $('#contact-button').on('click', function () {
-        $('.contact-box').slideToggle('slow');
+        $('#about-button').on('click', function () {
+            $('.about-box').slideToggle('slow');
+        });
+
+        $('#contact-button').on('click', function () {
+            $('.contact-box').slideToggle('slow');
+        });
+    }
+
+    // Bootstrap carousel
+
+    $('#picCarousel').carousel({
+        interval: 10000
+    })
+
+    $('.carousel .carousel-item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < 3; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+        }
     });
 });
